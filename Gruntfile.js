e9059a9b6835cd864c378fba70e41ff7ba4fa0ff
Vladimir Vershinin <vladimir.vershinin@tatar.ru>
2016-02-18 10:46:35 +0300

    fix UMD wrapper template, fix path to dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,9 @@ module.exports = function(grunt) {
             }
         },
         umd_wrapper: {
+            options: {
+                template: './src/umd-template'
+            },
             build: {
                 files: [{
                     src: '<%= paths.src %>/module',
